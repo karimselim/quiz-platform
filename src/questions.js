e422adb0dbb9477c809c24b0d850b86b15cbbfac
karim <kareemsleem7@gmail.com>
2024-06-16 14:08:30 +0300

    commiting quiz app

---
+++ src/questions.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: "q1",
+    text: "Which country has the largest population?",
+    answers: ["china", "India", "USA", "Russia"],
+  },
+  {
+    id: "q2",
+    text: "Which country is known as the Hexagon?",
+    answers: ["France", "Canada", "Morocco", "Spain"],
+  },
+  {
+    id: "q3",
+    text: "What country refers to themselves as ‘Kiwis’?",
+    answers: ["New Zealand", "India", "Argentina", "Mexico"],
+  },
+  {
+    id: "q4",
+    text: "What country has the longest coastline?",
+    answers: ["Canada", "Russia", "USA", "China"],
+  },
+  {
+    id: "q5",
+    text: "Which country has three capital cities—Pretoria, Cape Town, and Bloemfontein?",
+    answers: ["South Africa", "Nigeria", "Algeria", "Congo"],
+  },
+  {
+    id: "q6",
+    text: "Which continent is home to the most countries?",
+    answers: ["Africa", "Asia", "South America", "Europe"],
+  },
+  {
+    id: "q7",
+    text: "What is the largest country (by size and population) in South America?",
+    answers: ["Brazil", "Chile", "Argentina", "Colombia"],
+  },
+];
